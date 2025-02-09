9cce9385188ac407a6c7159d3ec49d60bd69e895
keiys <117022947+keiys@users.noreply.github.com>
2025-02-09 21:01:49 +0400

    Add files via upload

---
+++ src/Homework3_3.java
@@ -0,0 +1,30 @@
+/*public class Book {
+    String title;
+    String author;
+    int pages;
+}*/
+
+/*public class Employee {
+    String fullName;
+    String position;
+    int salary;
+}*/
+
+/*public class Product {
+    String name;
+    int price;
+    int quantity;
+}*/
+
+/*public class Handphone {
+    String brand;
+    String model;
+    int ram;
+    int storage;
+}*/
+
+/*public class Clothes {
+    String name;
+    String color;
+    int size;
+}*/
