280767b28105589b8659d2cc3a0caa863fa6d9d2
enima97 <kyu.keiys97@gmail.com>
2025-02-14 17:42:12 +0400

    adding new files

---
+++ src/model/Student.java
@@ -0,0 +1,69 @@
+package model;
+
+public class Student {
+    String name;
+    String surname;
+    int year = 2010;
+    char gender;
+    double mark;
+    boolean isPHD;
+
+    public void printInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Year: " + year);
+        System.out.println("Gender: " + (gender == 'F'? "Female": "Male"));
+        System.out.println("Mark: " + mark);
+        System.out.println("isPHD: " + (isPHD ? "Yes" : "No"));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if(year >= 1940 && year <= 2010){
+            this.year = year;
+        }
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public boolean isPHD() {
+        return isPHD;
+    }
+
+    public void setPHD(boolean PHD) {
+        isPHD = PHD;
+    }
+}
---
+++ src/service/StudentService.java
@@ -0,0 +1,77 @@
+package service;
+
+import model.Student;
+
+import java.util.Scanner;
+
+public class StudentService {
+
+    public Student createStudent() {
+        Scanner sc = new Scanner(System.in);
+        Student student = new Student();
+        System.out.println("Enter student name: ");
+        String name = sc.next();
+        System.out.println("Enter surname: ");
+        String surname = sc.next();
+        System.out.println("Enter year: ");
+        int year = sc.nextInt();
+        System.out.println("Enter gender: ");
+        char gender = sc.next().charAt(0);
+        System.out.println("Enter mark: ");
+        double mark = sc.nextDouble();
+        System.out.println("Enter PHD: ");
+        boolean phd = sc.nextBoolean();
+
+        student.setName(name);
+        student.setSurname(surname);
+        student.setYear(year);
+        student.setGender(gender);
+        student.setMark(mark);
+        student.setPHD(phd);
+        return student;
+    }
+
+    public void maxStudent(Student student1, Student student2) {
+        if (student1.getYear() < student2.getYear()) {
+            student1.printInfo();
+        }else {
+            student2.printInfo();
+        }
+    }
+
+    public void femaleStudents(Student[] student) {
+        for (int i = 0; i < student.length; i++) {
+            if (student[i].getGender() == 'F') {
+                student[i].printInfo();
+            }
+        }
+    }
+
+    public Student studentWithMaxMark(Student[] student) {
+        Student m = null;
+        for (int i = 0; i < student.length; i++) {
+            if(student[i].isPHD()) {
+                if(m == null || student[i].getMark() > m.getMark()) {
+                    m = student[i];
+                }
+            }
+        }
+        return m;
+    }
+
+    public Student[] sortStudentsByAge (Student[] student) {
+        Student[] sorted = new Student[student.length];
+        for (int i = 0; i < student.length; i++) {
+            sorted[i] = student[i];
+        }
+        for (int i = 1; i < sorted.length; i++) {
+            if (sorted[i].getYear() > sorted[i - 1].getYear()) {
+                Student temp = sorted[i];
+                sorted[i] = sorted[i - 1];
+                sorted[i - 1] = temp;
+            }
+        }
+        return sorted;
+    }
+
+}
